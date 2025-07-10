ce955e5167447fc59858bc29436f11a0fc9bca6a
B.V.S.Pavan <bvspavan234@gmail.com>
2025-07-11 02:43:07 +0530

    Updated Teams

--- frontend/src/components/Team/People.jsx
+++ frontend/src/components/Team/People.jsx
@@ -4,12 +4,12 @@ import '../Team/Team.css';
 import { Linkedin } from "lucide-react";
 
 const teamMembers = [
-    { name: "Pavan", year:"4th Year", branch: "CSE",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/Pavan_rlqrcw.png",portfolio:"https://pavan-portfolio-drab.vercel.app",instagram:"https://www.instagram.com/pavan_23_04/",linkedin:"https://www.linkedin.com/in/bvs-pavan-369752289/"},
+    { name: "Pavan", year:"4th Year", branch: "CSE",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/Pavan_rlqrcw.png",portfolio:"https://pavan-portfolio-drab.vercel.app",instagram:"https://www.instagram.com/__iampavan__23/",linkedin:"https://www.linkedin.com/in/bvs-pavan-369752289/"},
     { name: "Yoshitha", year:"4th Year", branch: "CSE-AIDS",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/Yoshitha_p157et.jpg"},
     { name: "Bhavana", year:"4th Year", branch: "CSE-AIML",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/Bhavana_uuatce.png"},
     { name: "Durga Prasad", year:"4th Year", branch: "CSE-AIDS",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/Durga_Prasad_gwl19i.jpg"},
     { name: "Youva Sri", year:"4th Year", branch: "CSE",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/Youva_Sri_x4jf2a.jpg"},
-    { name: "Jyothi", year:"4th Year", branch: "CSE",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/jaddu_jmap6u.jpg"},
+    { name: "Jyothi", year:"4th Year", branch: "CSE",profile:"https://res.cloudinary.com/dwb8m6n0y/image/upload/Team/jaddu_jmap6u.jpg",instagram:"https://www.instagram.com/pavan_23_04/"},
     { name: "Nanditha", year:"4th Year", branch: "CSE"},
     { name: "Ganga Raju", year:"4th Year", branch: "CSE"},
     { name: "Dinesh", year:"4th Year", branch: "CSE"},
